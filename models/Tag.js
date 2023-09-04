63c4b0e449468e7a1e22f0bc36cf8d572932d1bc
jimmyjr77 <jimmyjr.obrien@gmail.com>
2023-09-04 13:36:14 -0400

    added tag model

--- models/Tag.js
+++ models/Tag.js
@@ -1,20 +1,38 @@
 const { Model, DataTypes } = require('sequelize');
+const sequelize = require('../config/connection');
 
-const sequelize = require('../config/connection.js');
+class ProductTag extends Model {}
 
-class Tag extends Model {}
-
-Tag.init(
+ProductTag.init(
   {
-    // define columns
+    id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement: true,
+    },
+    product_id: {
+      type: DataTypes.INTEGER,
+      references: {
+        model: 'product',
+        key: 'id',
+      },
+    },
+    tag_id: {
+      type: DataTypes.INTEGER,
+      references: {
+        model: 'tag',
+        key: 'id',
+      },
+    },
   },
   {
     sequelize,
     timestamps: false,
     freezeTableName: true,
     underscored: true,
-    modelName: 'tag',
+    modelName: 'product_tag',
   }
 );
 
-module.exports = Tag;
+module.exports = ProductTag;
